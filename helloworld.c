bfc037ae86bb87ea46f6f0cab96cb9cb8479e7c1
prasoonmishra <prasoonmishra32@gmail.com>
2020-09-07 23:04:06 +0530

    Modified Helloworld.c for better readability

--- helloworld.c
+++ helloworld.c
@@ -3,6 +3,7 @@
 int main()
 {
 	#pragma omp parallel // It is OpenMP contruct which ask the number for no. threads from system( system decides the number thread to provide )
+	
 	{
 		int ID=omp_get_thread_num(); 
 		// it is a runtime routine which asks which thread it is
