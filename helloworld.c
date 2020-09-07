878c39be95568d148aee19c8f36613088571ccbd
prasoonmishra <prasoonmishra32@gmail.com>
2020-09-07 23:00:26 +0530

    Commiting helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include <omp.h>
+#include <stdio.h>
+int main()
+{
+	#pragma omp parallel // It is OpenMP contruct which ask the number for no. threads from system( system decides the number thread to provide )
+	{
+		int ID=omp_get_thread_num(); 
+		// it is a runtime routine which asks which thread it is
+		printf("hello(%d)",ID);
+		printf("world(%d)\n",ID);
+	}
+}
